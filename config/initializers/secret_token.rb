ee42c88aab56897c4bd10c1c8383f3198bcab658
GustavoCaso <gustavocaso@gmail.com>
2014-03-20 13:02:15 +0100

    * First COMMIT

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flycosette::Application.config.secret_key_base = '4431a20af4d99628f513962f368cf44f9b3f2e79021cc7ec2d4bfc208afd52106c0215926c9b99eff60c928237cfe3c40768700b7027baecbde1af1b6c1a5bab'

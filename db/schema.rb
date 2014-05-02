17d7ab4749f2c9583cc86f2564bbca9ea75409a6
Gustavo Caso <gustavocaso@gmail.com>
2014-05-02 13:53:02 +0200

    Finishing Admin section

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412093026) do
+ActiveRecord::Schema.define(version: 20140502114630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,5 +74,21 @@ ActiveRecord::Schema.define(version: 20140412093026) do
     t.datetime "photo_updated_at"
     t.text     "short_description"
   end
+
+  create_table "users", force: true do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "username"
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end

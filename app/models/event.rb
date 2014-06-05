afdf4607892e92c67a5c6291e6da2c0c65732412
GustavoCaso <gustavocaso@gmail.com>
2014-06-05 18:41:41 +0200

    Some changes in tha admin side

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,10 @@ class Event < ActiveRecord::Base
     }
 
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+  validates_presence_of :name, message: "Nombre no puede estar en blanco"
+  validates_presence_of :description, message: "La descripción no puede estar en blanco"
+  validates_presence_of :short_description, message: "La descrición corta no puede estar en blanco"
+  validates_presence_of :start_date, message: "La fecha de comienzo no puede estar en blanco"
 
   def date
     start_date.strftime("%Y-%B-%d")
